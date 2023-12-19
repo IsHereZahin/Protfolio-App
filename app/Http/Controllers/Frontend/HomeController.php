2f3361807e081477233b904b66ed4577d4ef779e
DeveloperZahin <abuzahinmohammadnowsin@gmail.com>
2023-12-19 17:02:42 +0600

    Hero View in frontend

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Hero;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,7 +14,8 @@ class HomeController extends Controller
     public function index()
     {
         //
-        return view('frontend.home.index');
+        $hero = Hero::first();
+        return view('frontend.home.index', compact('hero'));
     }
 
     /**
--- resources/views/frontend/home/section/hero.blade.php
+++ resources/views/frontend/home/section/hero.blade.php
@@ -1,16 +1,16 @@
   <!-- ======= Hero Section ======= -->
-  <section id="hero" class="d-flex flex-column justify-content-center">
+  <section id="hero" class="d-flex flex-column justify-content-center" style="background: url('{{ asset('Images/hero') }}/{{ $hero->image }}') top right no-repeat;" >
     <div class="container" data-aos="zoom-in" data-aos-delay="100">
-      <h1>Brandon Johnson</h1>
+      <h1>{{ $hero->name }}</h1>
       <p>I'm
         <span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span>
       </p>
       <div class="social-links">
-        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        <a href="{{ $hero->twitter_url }}" target="blank" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="{{ $hero->fb_url }}" target="blank" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="{{ $hero->in_url }}" target="blank" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="{{ $hero->sk_url }}" target="blank" class="google-plus"><i class="bx bxl-skype"></i></a>
+        <a href="{{ $hero->ln_url }}" target="blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
       </div>
     </div>
   </section><!-- End Hero -->
