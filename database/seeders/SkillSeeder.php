063a8a12cad43b4e5bdcf13c12a5134830ea9c0a
isherezahin <abuzahinmohammadnowsin@gmail.com>
2024-11-04 13:36:28 +0600

    Skill seeder

--- database/seeders/SkillSeeder.php
+++ database/seeders/SkillSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Skill; // Ensure to import the Skill model
 
 class SkillSeeder extends Seeder
 {
@@ -12,6 +12,37 @@ class SkillSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Sample data for skills
+        $skills = [
+            [
+                'name' => 'PHP',
+                'proficiency' => 85,
+            ],
+            [
+                'name' => 'JavaScript',
+                'proficiency' => 90,
+            ],
+            [
+                'name' => 'Laravel',
+                'proficiency' => 80,
+            ],
+            [
+                'name' => 'Vue.js',
+                'proficiency' => 75,
+            ],
+            [
+                'name' => 'Selenium',
+                'proficiency' => 70,
+            ],
+            [
+                'name' => 'Figma',
+                'proficiency' => 65,
+            ],
+        ];
+
+        // Insert data into the skills table
+        foreach ($skills as $skill) {
+            Skill::create($skill);
+        }
     }
 }
