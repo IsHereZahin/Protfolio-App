3b9f482a133812a959745814fe3105edec18a589
isherezahin <abuzahinmohammadnowsin@gmail.com>
2024-11-04 13:42:56 +0600

    class FactsSeeder extends Seeder

--- database/seeders/FactsSeeder.php
+++ database/seeders/FactsSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Fact; // Make sure to import the Fact model
 
 class FactsSeeder extends Seeder
 {
@@ -12,6 +12,20 @@ class FactsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Sample data for facts
+        $facts = [
+            [
+                'top_description' => 'Our Achievements',
+                'clients' => 100,
+                'projects' => 200,
+                'hours_of_support' => 5000,
+                'awards' => 10,
+            ],
+        ];
+
+        // Insert data into the facts table
+        foreach ($facts as $fact) {
+            Fact::create($fact);
+        }
     }
 }
